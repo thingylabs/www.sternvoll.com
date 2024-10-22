664cbde9312d84ac346cae0e2d5259da228f9693
idrathernotgetmails <idrathernotgetmails@gmail.com>
2024-10-22 08:28:18 +0000

    Remove old code/assets

--- old/assets/js/vamtam-all-js-extra.js
+++
@@ -1,4 +0,0 @@
-/*   <script type="text/javascript" id="vamtam-all-js-extra">
-    var VAMTAM_FRONT = { "ajaxurl": "https:\/\/bijoux.vamtam.com\/wp-admin\/admin-ajax.php", "jspath": "https:\/\/bijoux.vamtam.com\/wp-content\/themes\/bijoux\/vamtam\/assets\/js\/", "max_breakpoint": "1025", "beaver_responsive": "1025", "beaver_small": "768", "content_width": "1260", "enable_ajax_add_to_cart": "yes" };
-  </script> */
-var VAMTAM_FRONT = { "ajaxurl": "https:\/\/bijoux.vamtam.com\/wp-admin\/admin-ajax.php", "jspath": "https:\/\/bijoux.vamtam.com\/wp-content\/themes\/bijoux\/vamtam\/assets\/js\/", "max_breakpoint": "1025", "beaver_responsive": "1025", "beaver_small": "768", "content_width": "1260", "enable_ajax_add_to_cart": "yes" };
