21ad8cc0611480b581c656424bc0da74fd22e33a
idrathernotgetmails <idrathernotgetmails@gmail.com>
2024-10-22 16:59:03 +0000

    Fix Journal height

--- components/Journal.tsx
+++ components/Journal.tsx
@@ -6,39 +6,41 @@ interface JournalProps {
 
 export function Journal({ imageSrc, title, date }: JournalProps) {
   return (
-    <div class='flex justify-center items-center h-screen'>
-      {/* Container for the entire journal */}
-      <div class='relative'>
-        <div
-          class='absolute w-full h-full bg-gray-200 rounded-lg'
-          style={{ right: '-10px', bottom: '-10px' }}
-        />
-        <div
-          class='absolute w-full h-full bg-gray-300 rounded-lg'
-          style={{ right: '-5px', bottom: '-5px' }}
-        />
-
-        {/* Main book cover image */}
-        <div class='relative w-full h-full rounded-lg shadow-lg overflow-hidden'>
-          <img
-            src={imageSrc}
-            alt={title}
-            class='w-full h-full object-cover rounded-lg shadow-lg'
+    <section class='flex justify-center items-center min-h-screen w-full p-10'>
+      <div class='max-w-md'>
+        <div class='relative'>
+          {/* Grey background layers */}
+          <div
+            class='absolute w-full h-full bg-gray-200 rounded-lg'
+            style={{ right: '-10px', bottom: '-10px' }}
+          />
+          <div
+            class='absolute w-full h-full bg-gray-300 rounded-lg'
+            style={{ right: '-5px', bottom: '-5px' }}
           />
 
-          <div class='absolute inset-0 flex flex-col justify-between items-center p-6'>
-            {/* Title */}
-            <h2 class='text-white text-xl font-serif tracking-wide mt-4'>
-              <img src='Sternvoll-bright.png' alt='Sternvoll Logo' />
-            </h2>
+          {/* Main book cover image */}
+          <div class='relative w-full h-full rounded-lg shadow-lg overflow-hidden'>
+            <img
+              src={imageSrc}
+              alt={title}
+              class='w-full h-full object-cover rounded-lg shadow-lg'
+            />
 
-            {/* Date at the bottom */}
-            <p class='text-lg font-accent tracking-widest text-white mb-4'>
-              {date}
-            </p>
+            <div class='absolute inset-0 flex flex-col justify-between items-center p-6'>
+              {/* Title */}
+              <h2 class='text-white text-xl font-serif tracking-wide mt-4'>
+                <img src='Sternvoll-bright.png' alt='Sternvoll Logo' />
+              </h2>
+
+              {/* Date at the bottom */}
+              <p class='text-lg font-accent tracking-widest text-white mb-4'>
+                {date}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
