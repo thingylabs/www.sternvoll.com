8e6bfcfde81bc36bdde4030567523cad0d7bd445
idrathernotgetmails <idrathernotgetmails@gmail.com>
2024-12-09 06:42:21 +0000

    Move include to deno.json -- fix build?

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import { type Config } from 'tailwindcss'
-import aspectRatio from 'https://esm.sh/@tailwindcss/aspect-ratio@0.4.0'
+import aspectRatio from 'aspectRatio'
 
 export default {
   theme: {
